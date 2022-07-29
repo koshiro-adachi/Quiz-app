a1968e728d15c1121ecc6b8fe8bb9138fa8ec6f0
koshiro-adachi <107029087+koshiro-adachi@users.noreply.github.com>
2022-07-29 17:42:28 +0900

    save

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,5 +1,28 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 export const Question: FC = memo(() => {
-  return <div>Question</div>;
+  return (
+    <Flex bg="blue.200" height="100vh" m={4} borderRadius="20" justify="center">
+      <Box>
+        <Box textAlign="center">
+          <Heading color="white">問題</Heading>
+        </Box>
+        <Flex
+          bg="white"
+          height="75vh"
+          w="70vw"
+          borderRadius="md"
+          fontSize="large"
+          fontWeight="1000"
+          color="gray.500"
+          p={5}
+          textAlign="center"
+          justify="center"
+        >
+          Q.プログラミングは楽しいか否か？
+        </Flex>
+      </Box>
+    </Flex>
+  );
 });
